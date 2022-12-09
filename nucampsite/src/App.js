dae786af1d256fb6b3485f3761db8533469b527b
jeedeekay <gavinkinder@gmail.com>
2022-12-09 09:46:24 -0800

    Header and Footer Links

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,3 +1,6 @@
+import { Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ContactPage from './pages/ContactPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDirectoryPage from './pages/CampsiteDirectoryPage';
@@ -7,7 +10,11 @@ function App() {
   return (
     <div className="App">
       <Header />
-      <CampsiteDirectoryPage />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='contact' element={<ContactPage />} />
+        <Route path='directory' element={<CampsiteDirectoryPage />} />
+      </Routes>
       <Footer />
     </div>
   );
