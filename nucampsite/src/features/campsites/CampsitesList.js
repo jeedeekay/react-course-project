39df65745a9edf479e0e5e286b54dccfd52e479b
jeedeekay <gavinkinder@gmail.com>
2022-12-22 11:11:41 -0800

    Campsites useSelector

--- nucampsite/src/features/campsites/CampsitesList.js
+++ nucampsite/src/features/campsites/CampsitesList.js
@@ -1,14 +1,15 @@
-import { CAMPSITES } from '../../app/shared/CAMPSITES';
+import { useSelector } from 'react-redux';
 import { Col, Row, } from 'reactstrap';
 import CampsiteCard from './CampsiteCard';
 import { selectAllCampsites } from './campsitesSlice';
 
 const CampsitesList = () => {
-    const campsites = selectAllCampsites();    
+    const campsites = useSelector(selectAllCampsites); 
+    console.log('campsites:', campsites);
 
     return (
         <Row className='ms-auto'>
-            {CAMPSITES.map((campsite) => {
+            {campsites.map((campsite) => {
                 return (
                     <Col md='5' className='m-4' key={campsite.id}>
                         <CampsiteCard campsite={campsite} />
