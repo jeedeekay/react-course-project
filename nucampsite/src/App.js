e3478055a49e24c7537ee29b26de5011c5af02bf
jeedeekay <gavinkinder@gmail.com>
2022-12-28 10:50:09 -0800

    Async Campsites

--- nucampsite/src/App.js
+++ nucampsite/src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ContactPage from './pages/ContactPage';
@@ -6,9 +8,16 @@ import Footer from './components/Footer';
 import CampsiteDirectoryPage from './pages/CampsiteDirectoryPage';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import AboutPage from './pages/AboutPage';
+import { fetchCampsites } from './features/campsites/campsitesSlice';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
   return (
     <div className='App'>
       <Header />
